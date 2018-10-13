aed4c7f47808e14354d1f7ab51c81357a36909d6
Adris <adrisj7@gmail.com>
2018-10-13 17:40:46 -0400

    Add entirely new project folder which we will use from now on

---
+++ LaserTag/app/src/main/java/com/lasertag/mhacks11/lasertag/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lasertag.mhacks11.lasertag;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
